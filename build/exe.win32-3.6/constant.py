1a970e7ce35950405ca8b2d17defacfba0f98305
LudoHazred <ludovic.gros2@gmail.com>
2017-10-05 15:29:34 +0200

    add standalone version in the build folder

---
+++ build/exe.win32-3.6/constant.py
@@ -0,0 +1,25 @@
+"""
+Constants of Macgyver labyrinth's game
+Ludovic GROS
+"""
+
+# Window parameters (tout mettre en majuscule sur les constantes)
+SPT_NB_SD = 15
+SPT_SZ = 40
+WINDOW_SIDE = SPT_NB_SD * SPT_SZ
+
+# Images lists
+HOMEPAGE = "images/homepage.png"
+BACKGROUND = "images/background.png"
+WALL_IMG = "images/wall_tile.png"
+FLOOR_IMG = "images/floor_tile.png"
+FLOOR_F = "images/floor_final_tile.png"
+INVENTORY = "images/inventory.png"
+GUARDIAN_IMG = "images/guardian.png"
+MACGYVER_IMG = "images/macgyver.png"
+ITEM_IMG = "images/object.png"
+ITEM2_IMG = "images/object2.png"
+ITEM3_IMG = "images/object3.png"
+ITEM4_IMG = "images/object_final.png"
+WIN = "images/free.png"
+LOOSE = "images/loose.png"
